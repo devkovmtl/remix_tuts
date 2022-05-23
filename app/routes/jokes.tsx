2cc6bd50d877ccf39502f25cce538e33b4a3be73
Mikael <dev.kov.mtl@gmail.com>
2022-05-23 15:11:11 -0400

    Nested Routes

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from '@remix-run/react';
+
 export default function Jokes() {
   return (
     <div>
       <h1>J🤪kes</h1>
+      <Outlet />
     </div>
   );
 }
---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,11 @@
+export default function JokesIndexRoute() {
+  return (
+    <div>
+      <p>Random jokes</p>
+      <p>
+        I was wondering why the frisbee was getting biggr, then it hit me!
+        hahaha!
+      </p>
+    </div>
+  );
+}

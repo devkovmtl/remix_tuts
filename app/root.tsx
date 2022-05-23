a78e3510ada8189a8a4f9a7de7f0cfc9d687aa0a
Mikael <dev.kov.mtl@gmail.com>
2022-05-23 16:05:35 -0400

    Adding remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,31 @@
 // LiveReload:hot reload the app
 // Outlet for route
-import { LiveReload, Outlet } from '@remix-run/react';
+// Links: show the links
+import { Links, LiveReload, Outlet } from '@remix-run/react';
+import { LinksFunction } from '@remix-run/node';
+
+import globalStylesUrl from './styles/global.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
+import globalLargeStylesUrl from './styles/global-large.css';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStylesUrl,
+    },
+    {
+      rel: 'stylesheet',
+      href: globalMediumStylesUrl,
+      media: 'print, (min-width: 640px)',
+    },
+    {
+      rel: 'stylesheet',
+      href: globalLargeStylesUrl,
+      media: 'screen and (min-width: 1024px)',
+    },
+  ];
+};
 
 // root of the app
 export default function App() {
@@ -9,6 +34,7 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <title>Remix Jokes App</title>
+        <Links />
       </head>
       <body>
         <Outlet />
